9d01bfb0c7c9d216d445768d32c2d77eb8d43384
Antonio Ruiz <3speedemon3@gmail.com>
2023-12-08 19:19:31 +0100

    class main finished

--- _01/Main.java
+++ _01/Main.java
@@ -1,9 +1,27 @@
 package _01;
-
+/**
+ * @author Antonio Ruiz Benito -> SpeedDemoN
+ * program main class
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Team team = new Team("Real Madrid", 2023, 10_000_000.33);
+		
+		Player zidane = new Player("Zidane", "Forward", 5);
+		Player cr7 = new Player("C.Ronaldo", "Forward", 7);
+		Player benzema = new Player("Benzema", "Forward", 9);
+				
+		team.addPlayer(zidane);
+		team.addPlayer(cr7);
+		team.addPlayer(benzema);
+		
+		System.out.println(team.showPlayers());
+		System.out.println(zidane.toString());
+		System.out.println(cr7.toString());
+		System.out.println(benzema.toString());
+
 
 	}
 
